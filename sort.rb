7a4129cb450fdff719c6c1f27fb9828403d9d358
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-09 13:36:51 -0400

    bubble_sort complete

--- sort.rb
+++ sort.rb
@@ -21,14 +21,29 @@ class Sorter
   def insertion_sort(array)
 
     0.upto(array.length - 2) do |i|
-      puts "i = #{i}"
-      puts "value = #{array[i]}"
       insert(array, i, array[i + 1])
     end
 
   end
 
   def bubble_sort(array)
+
+    # [3, 5, 1, 2, 9, 6, 4, 8, 7]
+
+    # 0.upto(array.length - 1) do |i|
+    #   i.upto(array.length-1) do |j|
+    #     array[i], array[j] = array[j], array[i] if array[i] > array[j]
+    #   end
+    #   p array
+    # end
+
+    0.upto(array.length-2) do |i|
+      0.upto((array.length-2)-i) do |j|
+        array[j],array[j+1] = array[j+1], array[j] if array[j] > array[j + 1]
+      end
+      p array
+    end
+
   end
 
 end
